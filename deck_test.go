ba1a385f1e5d94621ed09ce0d587ae2f54552014
rehankhan21 <narutop224@gmail.com>
2023-03-29 00:58:53 -0400

    added testing for i/o files

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,4 +19,22 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d) - 1] != "Four of Clubs" {
 		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d) - 1])
 	}
+
+	// no hand holding in GO testing framwork, if test fails we need to delete files oursleves
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	// delete any test files that were left over from a failed test run
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
